21c20bcb819ff1c3b1a39666dd8a4e7c390c0084
doxuantruong120605 <truongdxth2307038@fpt.edu.vn>
2024-01-25 14:03:54 +0700

    commit

---
+++ class1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a , b ,sum;
+    printf("enter the first number:  ");
+    scanf("%d" , &a);
+    printf("enter the first number:  ");
+    scanf("%d" , &b);
+    sum = a + b;
+    printf("The sum is : %d" , sum);
+    return 0;
+
+}
