1f10f3849027bca06f3dd43c0d97022bedf48572
doxuantruong120605 <truongdxth2307038@fpt.edu.vn>
2024-01-25 17:11:55 +0700

    timso

---
+++ class1/timso.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+	int num , res ;
+		printf("Enter Num : ");
+		scanf("%d", &num);
+		res = num % 2;
+		if ( res == 0) {
+	
+			printf("So chan");
+			
+		}else{
+		 
+		
+			printf(" so le");
+		}	
+			
+	return 0;
+			
+		
+	
+}
